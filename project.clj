ec8debd619ffab97f1f82461c88381186704e000
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-25 17:30:22 +0400

    bump clojure version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "pattern count. Bioinformatics algorithms"
   :main pattern_count.core
   :dependencies [
-                  [org.clojure/clojure "1.3.0"]
+                  [org.clojure/clojure "1.4.0"]
                   [clj-getopts "0.0.2"]
                   [org.clojure/tools.trace "0.7.5"]
                 ])
