65e815bc072dce76385621b03e6415698d422e71
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-25 17:33:30 +0400

    add tests

--- test/pattern_count/test/core.clj
+++ test/pattern_count/test/core.clj
@@ -1,6 +1,23 @@
 (ns pattern_count.test.core
+  (:import [java.io BufferedReader StringReader])
   (:use [pattern_count.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest replace-me
+  (let [
+        [line1 line2] (read-lines
+                       (BufferedReader. (StringReader.
+                                         "QWER\nASDF\nZXCV\n\n3421"))
+                       0)
+        [line3 line4] (read-lines
+                       (BufferedReader. (StringReader.
+                                         "QWER\nASDF\nZXCV\n\n3421"))
+                       1)
+        ]
+    (is (= line1 "QWER"))
+    (is (= line2 "ASDF"))
+    (is (= line3 "ASDF"))
+    (is (= line4 "ZXCV"))
+    )
+  )
+
