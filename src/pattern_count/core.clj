1de1b5c3dd87548282b9f33944b64ba5d5334d7a
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-25 17:33:21 +0400

    split file read

--- src/pattern_count/core.clj
+++ src/pattern_count/core.clj
@@ -8,15 +8,33 @@
 
 ;;(trace-ns 'pattern_count.filter)
 
-(defn read-data [opts]
-  (let [fname (get opts :infile)
-        acc (with-open [fd (clojure.java.io/reader fname)]
-              (let [[line1 line2] (take 2 (line-seq fd))]
-                (pattern_count.filter/find-items line1 line2)))]
+(defn read-lines [fd skip]
+  (let [
+        lines (line-seq fd)
+        lines2 (drop skip lines)
+        [line1 line2] (take 2 lines2)
+        res [line1 line2]
+        ;; res (pattern_count.filter/find-items line1 line2)
+        ]
+    res))
+
+(defn read-file [fname skip]
+  (let [acc (with-open [fd (clojure.java.io/reader fname)]
+              (doall
+               (read-lines fd skip)
+               ))]
     acc))
 
+(defn read-data [opts]
+  (let [fname (get opts :infile)
+        skip-str (get opts :skip "0")
+        skip (Integer/parseInt skip-str)
+        ]
+    (read-file fname skip)))
+
 (defn -main [& args]
-  (let [opts (getopts (options "i?" {:infile :arg}) args)
+  (let [opts (getopts (options "is" {:infile :arg
+                                     :skip :arg}) args)
         data (read-data opts)]
     (println "data:" data)))
 
