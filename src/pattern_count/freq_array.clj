aaf41a201174c1182cc76dfb7c2763b9569f1766
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-26 14:48:52 +0400

    add freq_array

---
+++ src/pattern_count/freq_array.clj
@@ -0,0 +1,14 @@
+(ns pattern_count.freq_array
+  (:require bia_utils.util)
+  )
+
+(defn count-k-mers-aux [idx k acc len line]
+  (if (bia_utils.util/is-data-available idx len k)
+    (count-k-mers-aux (inc idx) k
+                      (bia_utils.util/add-one-k-mer idx line k acc)
+                      len line)
+    acc))
+
+(defn count-k-mers [line k]
+  (count-k-mers-aux 0 k {} (count line) line))
+
