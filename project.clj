67b74661838c26c52ec50cdfa1da8432c6353b8e
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-26 14:48:37 +0400

    add util dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject pattern_count "1.0.0-SNAPSHOT"
   :description "pattern count. Bioinformatics algorithms"
   :main pattern_count.core
+  :extra-classpath-dirs [
+                         ".lein-git-deps/bia_utils/src"
+                         ]
   :dependencies [
                   [org.clojure/clojure "1.4.0"]
                   [clj-getopts "0.0.2"]
