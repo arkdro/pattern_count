c526e0501f82c4bb099594f3a3e7fd3349127c1f
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-27 23:23:25 +0300

    add tests for pattern_count/find_clump

---
+++ test/pattern_count/test/find_clump.clj
@@ -0,0 +1,26 @@
+(ns pattern_count.test.find_clump
+  ;; (:use clojure.tools.trace)
+  (:use [pattern_count.find_clump])
+  (:use [clojure.test]))
+
+;; (trace-ns 'bia_utils.util)
+;; (trace-ns 'pattern_count.find_clump)
+
+(deftest store-k-mer-position-test
+  (is (= {"asa" [0]}
+         (pattern_count.find_clump/store-k-mer-position
+          0 "asadfasasa" 3 4 1 {})))
+  (is (= {"asa" [0 5]}
+         (pattern_count.find_clump/store-k-mer-position
+          5 "asadfasasa" 3 4 1 {"asa" [0]})))
+  (is (= {"dfa" [3] "asa" [0]}
+         (pattern_count.find_clump/store-k-mer-position
+          3 "asadfasasa" 3 4 1 {"asa" [0]})))
+  )
+
+(deftest collect-k-mers-positions-test
+  (is (= {"sas" [6], "fas" [4], "dfa" [3], "adf" [2], "sad" [1], "asa" [0 5 7]}
+         (pattern_count.find_clump/collect-k-mers-positions
+          0 3 5 2 {} 10 "asadfasasa")))
+  )
+
