956f0819e4db57eceeeb0ce2799242d5ef9e0a0e
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-30 00:11:16 +0300

    pass k-mer length further

--- src/pattern_count/find_clump.clj
+++ src/pattern_count/find_clump.clj
@@ -19,41 +19,41 @@
            len line)
     acc))
 
-(defn is-distance-short [window cur-pos next-pos]
+(defn is-distance-short [k window cur-pos next-pos]
   (let [delta (- next-pos cur-pos)]
     (< delta window)))
 
-(defn is-next-pos-good-for-clump [window idx cur-pos positions]
+(defn is-next-pos-good-for-clump [k window idx cur-pos positions]
   (cond
     (empty? positions) false
     (<= (count positions) idx) false
-    :default (is-distance-short window cur-pos (nth positions idx))))
+    :default (is-distance-short k window cur-pos (nth positions idx))))
 
 ;; results should be processed as a such:
 ;; found - continue, starting from the rest of positions (t positions consumed)
 ;; that are farther by window size (for this window position the clump
 ;; has been found) from cur-pos. In fact, continue is unnecessary.
 ;; not found - continue, starting from the second position of the whole list
-(defn get-clump-aux2 [window need k-mer cnt cur-pos positions]
+(defn get-clump-aux2 [k window need k-mer cnt cur-pos positions]
   (cond
     (>= cnt need) {:found true, :cnt cnt, :pos cur-pos, :rest positions}
-    (is-next-pos-good-for-clump window cnt cur-pos positions)
-      (recur window need k-mer (inc cnt) cur-pos positions)
+    (is-next-pos-good-for-clump k window cnt cur-pos positions)
+      (recur k window need k-mer (inc cnt) cur-pos positions)
     :default {:found false}))
 
-(defn get-clump-aux1 [window need k-mer positions]
+(defn get-clump-aux1 [k window need k-mer positions]
   (if (empty? positions)
     [k-mer nil]
-    (let [res (get-clump-aux2 window need k-mer 1
+    (let [res (get-clump-aux2 k window need k-mer 1
                               (first positions)
                               positions)]
       (if (:found res)
         [k-mer (:pos res)]
-        (recur window need k-mer (vec (rest positions)))))))
+        (recur k window need k-mer (vec (rest positions)))))))
 
-(defn get-clump [window need [k-mer positions]]
+(defn get-clump [k window need [k-mer positions]]
   (if (>= (count positions) need)
-    (get-clump-aux1 window need k-mer positions)
+    (get-clump-aux1 k window need k-mer positions)
     [k-mer nil]
     )
   )
@@ -61,7 +61,7 @@
 (defn iter-k-mers [line k window t]
   (let [
         positions (collect-k-mers-positions 0 k window t {} (count line) line)
-        raw-clumps (map #(get-clump window t %) positions)
+        raw-clumps (map #(get-clump k window t %) positions)
         clumps (filter #(not (nil? (second %))) raw-clumps)
         ]
     clumps)
