90c942dc6f833ce0d834ccdf3cd66b192e6b9d27
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-30 00:11:30 +0300

    update tests

--- test/pattern_count/test/find_clump.clj
+++ test/pattern_count/test/find_clump.clj
@@ -25,63 +25,63 @@
   )
 
 (deftest is-distance-short-test
-  (is (= true (pattern_count.find_clump/is-distance-short 10 0 2)))
-  (is (= false (pattern_count.find_clump/is-distance-short 10 0 12)))
-  (is (= false (pattern_count.find_clump/is-distance-short 10 0 10)))
+  (is (= true (pattern_count.find_clump/is-distance-short 3 10 0 2)))
+  (is (= false (pattern_count.find_clump/is-distance-short 3 10 0 12)))
+  (is (= false (pattern_count.find_clump/is-distance-short 3 10 0 10)))
   )
 
 (deftest is-next-pos-good-for-clump-test
   (is (= false
          (pattern_count.find_clump/is-next-pos-good-for-clump
-          10 0 2 [])))
+          3 10 0 2 [])))
   (is (= false
          (pattern_count.find_clump/is-next-pos-good-for-clump
-          10 1 5 [5])))
+          3 10 1 5 [5])))
   (is (= true
          (pattern_count.find_clump/is-next-pos-good-for-clump
-          10 1 2 [0 5])))
+          3 10 1 2 [0 5])))
   (is (= false
          (pattern_count.find_clump/is-next-pos-good-for-clump
-          10 3 7 [0 5 7 32])))
+          3 10 3 7 [0 5 7 32])))
   )
 
 (deftest get-clump-aux2-test
   (is (= {:found false}
          (pattern_count.find_clump/get-clump-aux2
-          10 2 "asa" 1 0 [0 13 17])))
+          3 10 2 "asa" 1 0 [0 13 17])))
   (is (= {:found true, :cnt 1, :pos 0, :rest [0 5 7]}
          (pattern_count.find_clump/get-clump-aux2
-          10 1 "asa" 1 0 [0 5 7])))
+          3 10 1 "asa" 1 0 [0 5 7])))
   (is (= {:found true, :cnt 2, :pos 0, :rest [0 5 27]}
          (pattern_count.find_clump/get-clump-aux2
-          10 2 "asa" 1 0 [0 5 27])))
+          3 10 2 "asa" 1 0 [0 5 27])))
   (is (= {:found false}
          (pattern_count.find_clump/get-clump-aux2
-          10 2 "asa" 1 5 [5 27])))
+          3 10 2 "asa" 1 5 [5 27])))
   )
 
 (deftest get-clump-aux1-test
   (is (= ["asa" nil]
          (pattern_count.find_clump/get-clump-aux1
-          10 2 "asa" [0 13 27])))
+          3 10 2 "asa" [0 13 27])))
   )
 
 (deftest get-clump-test
   (is (= ["asa" nil]
          (pattern_count.find_clump/get-clump
-          10 2 ["asa" []])))
+          3 10 2 ["asa" []])))
   (is (= ["asa" nil]
          (pattern_count.find_clump/get-clump
-          10 2 ["asa" [0]])))
+          3 10 2 ["asa" [0]])))
   (is (= ["asa" nil]
          (pattern_count.find_clump/get-clump
-          10 2 ["asa" [0 13 27]])))
+          3 10 2 ["asa" [0 13 27]])))
   (is (= ["asa" 0]
          (pattern_count.find_clump/get-clump
-          10 2 ["asa" [0 5 27]])))
+          3 10 2 ["asa" [0 5 27]])))
   (is (= ["asa" 15]
          (pattern_count.find_clump/get-clump
-          10 2 ["asa" [0 15 22]])))
+          3 10 2 ["asa" [0 15 22]])))
   )
 
 (deftest iter-k-mers-test
